f428d532df0e02a51ed7cdbab9aa686f03e752fe
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2024-01-28 16:13:55 +0800

    Video-15-Improve_Add_To
    	modified:   frontend/src/screens/ProductScreen.js
    
        made some altercations and makeshift solutions to image discrepancy
        when the image is not showing when the user is redirected to the product itself

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -59,26 +59,39 @@ function ProductScreen() {
         image = "http://localhost:3000/images/p1.jpg"
     } else if (product.name === "Adidas Fit Shirt") {
         image = "http://localhost:3000/images/p2.jpg"
-    } else if (product.name === "Nike Slim Pant") {
-        image = "http://localhost:3000/images/p3.jpg"
-    } else if (product.name === "Adidas Fit Pant") {
-        image = "http://localhost:3000/images/p4.jpg"
-    } else if (product.name === "Adidas Fit Pant") {
-        image = "http://localhost:3000/images/p4.jpg"
-    } else if (product.name === "Adidas Fit Pant") {
-        image = "http://localhost:3000/images/p4.jpg"
+    }
+    //  else if (product.name === "Nike Slim Pant") {
+    //     image = "http://localhost:3000/images/p3.jpg"
+    // } else if (product.name === "Adidas Fit Pant") {
+    //     image = "http://localhost:3000/images/p4.jpg"
+    // } else if (product.name === "Adidas Fit Pant") {
+    //     image = "http://localhost:3000/images/p4.jpg"
+    // } else if (product.name === "Adidas Fit Pant") {
+    //     image = "http://localhost:3000/images/p4.jpg"
+    // } 
+    else {
+
     }
 
 
 
     const { state, dispatch: ctxDispatch } = useContext(Store);
-    const addToCartHandler = () => {
+    const { cart } = state;
+    const addToCartHandler = async () => {
+        const existItem = cart.cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${product._id}`);
+        if (data.countInStock < quantity) {
+            window.alert('Sorry. Out of Stock');
+            return;
+        }
         ctxDispatch({
             type: 'CART_ADD_ITEM',
-            payload: { ...product, quantity: 1 },
+            payload: { ...product, quantity },
         });
     };
 
+    console.log(`http://localhost:3000/${product.image}`)
     return loading ? (
         <LoadingBox />
     ) : error ? (
@@ -87,7 +100,7 @@ function ProductScreen() {
         <div>
             <Row>
                 <Col md={6}>
-                    <img src={`${image}` || product.image} alt={product.name} className="img-large" />
+                    <img src={`${image}` || `http://localhost:3000/${product.image}` || product.image} alt={product.name} className="img-large" />
                 </Col>
                 <Col md={3}>
                     <ListGroup variant="flush">
