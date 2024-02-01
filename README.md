d08d3d4d3075c7490cbaead0951c9dbba13f6b5c
MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
2024-02-01 15:16:26 +0800

    Video-26-Create_Payment_Screen

--- README.md
+++ README.md
@@ -110,8 +110,13 @@
     3. add checkout wizard bar
 
 25. Create Sign Up Screen
+
     1. create input forms
     2. handle submit
     3. create backend api
+
+26. Implement Select Payment Method Screen
+    1. create input forms
+    2. handle submit
 
 <!-- PROJECT WEBSITE = https://amazona.onrender.com/ -->
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,8 +16,7 @@ import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
-
-
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -27,6 +26,7 @@ function App() {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
   };
 
   return (
@@ -83,8 +83,8 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
-              <Route path="/shipping" element={<ShippingAddressScreen />}
-              ></Route>
+              <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
